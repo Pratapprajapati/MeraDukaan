bde49c7b94af70ef9419a7e68f4c98c7d8755c9e
Vignesh-604 <vig6604@gmail.com>
2024-07-26 16:10:35 +0530

    Frontend setup complete

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import logo from "./assets/MeraDukan.logo-png.png"
+
+function App() {
+
+
+  return (
+    <div>
+      <img src={logo} alt="Ummmm" className='logo h-64 mx-auto' />
+      <h1 className=' text-7xl font-bold'>Mera Dukaan</h1>
+      <h2 className=' text-2xl mt-2'>Online local community marketplace</h2>
+    </div>
+  )
+}
+
+export default App
